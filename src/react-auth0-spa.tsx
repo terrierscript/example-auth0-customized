41f6483a23b7747426ea27b3d6592ec0145525e1
terrierscript <13282103+terrierscript@users.noreply.github.com>
2019-07-14 21:21:41 +0900

    This is my answer

--- src/react-auth0-spa.tsx
+++ src/react-auth0-spa.tsx
@@ -1,12 +1,17 @@
-import React, { useState, useEffect, useContext } from "react"
+import React, { FC, useState, useEffect, useContext, useCallback } from "react"
 import createAuth0Client from "@auth0/auth0-spa-js"
 
 const DEFAULT_REDIRECT_CALLBACK = () =>
   window.history.replaceState({}, document.title, window.location.pathname)
 
-export const Auth0Context = React.createContext()
+export const Auth0Context = React.createContext<undefined | unknown>(undefined)
 export const useAuth0 = () => useContext(Auth0Context)
-export const Auth0Provider = ({
+
+export const Auth0Provider: FC<
+  {
+    onRedirectCallback?: (appState: any) => void
+  } & Auth0ClientOptions
+> = ({
   children,
   onRedirectCallback = DEFAULT_REDIRECT_CALLBACK,
   ...initOptions
